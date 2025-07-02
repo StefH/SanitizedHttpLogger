b9f42e67077f4bd024e02914ba9cec08d7500b00
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-07-02 07:54:06 +0200

    c

--- src/SanitizedHttpClientLogger/Services/IRequestUriReplacer.cs
+++ src/SanitizedHttpClientLogger/Services/IRequestUriReplacer.cs
@@ -2,5 +2,10 @@
 
 internal interface IRequestUriReplacer
 {
+    /// <summary>
+    /// Replaces or sanitizes the given <see cref="Uri"/> and returns the result.
+    /// </summary>
+    /// <param name="uri">The original URI to be replaced or sanitized.</param>
+    /// <returns>The sanitized Uri, or sanitized string if the Uri cannot be constructed or null if input is null.</returns>
     object? Replace(Uri? uri);
 }
